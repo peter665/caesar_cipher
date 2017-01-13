787769a81580ed0db4e54cb0176b7bd98bcb05b6
Peter <peto.mester@gmail.com>
2017-01-13 21:50:26 +0100

    Corrected

--- caesar.rb
+++ caesar.rb
@@ -2,9 +2,13 @@ def caesar_cipher str, n
   letters = ('a'..'z').to_a
   result = ""
   str.split("").each do |s|
-    index = letters.index(s).to_i+n
-    index = index > letters.length-1 ? index - 26 : index  
-    result = result + letters[index]
+    if s.match(/[a-zA-Z]/)
+      index = letters.index(s.downcase).to_i+n
+      index = index > letters.length-1 ? index - 26 : index
+      result = result + letters[index]
+    else
+      result = result + s
+    end
   end
   result
 end
